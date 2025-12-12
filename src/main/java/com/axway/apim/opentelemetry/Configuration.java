e42c3078e9275f40a107d715a586f6828160c104
Julius Loman <julius.loman@alanata.sk>
2025-12-12 15:08:00 +0100

    Load Dynatrace metadata into OpenTelemetry resource

--- src/main/java/com/axway/apim/opentelemetry/Configuration.java
+++ src/main/java/com/axway/apim/opentelemetry/Configuration.java
@@ -1,8 +1,17 @@
 package com.axway.apim.opentelemetry;
 
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.instrumentation.runtimemetrics.java8.*;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Configuration {
 
@@ -21,8 +30,23 @@ public class Configuration {
 
     private static OpenTelemetry initOpenTelemetry() {
 
-        OpenTelemetry sdk = AutoConfiguredOpenTelemetrySdk.initialize()
-            .getOpenTelemetrySdk();
+        OpenTelemetrySdk sdk = AutoConfiguredOpenTelemetrySdk.builder().addResourceCustomizer((resource, properties) -> {
+            Resource dtMetadata = Resource.empty();
+
+            for (String name : new String[]{"dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties", "/var/lib/dynatrace/enrichment/dt_metadata.properties"}) {
+                try {
+                    Properties props = new Properties();
+                    props.load(name.startsWith("/var") ? new FileInputStream(name) : new FileInputStream(Files.readAllLines(Paths.get(name)).get(0)));
+                    dtMetadata = dtMetadata.merge(Resource.create(props.entrySet().stream()
+                            .collect(Attributes::builder, (b, e) -> b.put(e.getKey().toString(), e.getValue().toString()), (b1, b2) -> b1.putAll(b2.build()))
+                            .build())
+                    );
+                } catch (IOException e) {
+                }
+            }
+            return resource.merge(dtMetadata);
+        }).build().getOpenTelemetrySdk();        
+
         Classes.registerObservers(sdk);
         Cpu.registerObservers(sdk);
         MemoryPools.registerObservers(sdk);
